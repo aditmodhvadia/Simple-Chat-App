c4a3bda51f7715c3183e685043b4752dea664c40
Adit Modhvadia <dev.aditmodhvadia@gmail.com>
2020-10-20 12:34:19 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
