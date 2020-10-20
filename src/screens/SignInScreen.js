8c20c5e9cdc3b73c31af26a228a16bfc3fa175ea
Adit Modhvadia <adit.modhvadia@gmail.com>
2020-10-20 17:29:31 -0400

    - Add Google sign in button, authenticate user with google auth provider

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import GoogleButton from 'react-google-button'
+import firebase from 'firebase/app'
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+
+
 
 function SignInScreen() {
+    const [user] = useAuthState(firebase.auth())
+    console.log(user);
+
+    const handleGoogleSignIn = () => {
+        const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
+        firebase.auth().signInWithPopup(googleAuthProvider)
+    }
     return (
         <div>
-            Sign in screen
+            <GoogleButton onClick={handleGoogleSignIn} />
         </div>
     )
 }
