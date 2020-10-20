688fc2be86ad4057138d716f7310651317da8009
Adit Modhvadia <adit.modhvadia@gmail.com>
2020-10-20 17:36:08 -0400

    - Remove console log user

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import GoogleButton from 'react-google-button'
 import firebase from 'firebase/app'
-import { useAuthState } from 'react-firebase-hooks/auth';
-
-
 
 
 function SignInScreen() {
-    const [user] = useAuthState(firebase.auth())
-    console.log(user);
-
     const handleGoogleSignIn = () => {
         const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
         firebase.auth().signInWithPopup(googleAuthProvider)
