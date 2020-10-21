61462954135d5da35addb7a78108d479d435b270
Adit Modhvadia <adit.modhvadia@gmail.com>
2020-10-21 18:05:49 -0400

    - Add SendMessage which sends msgs to the chat room

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { SendMessage } from '../components/SendMessage'
+
 
 function ChatScreen() {
+    const chatMessagesRef = firebase.firestore().collection('chatmessages')
+    const query = chatMessagesRef.orderBy('createdAt').limit(50)
+
+    const [messages] = useCollectionData(query, { idField: 'id' })
+
     return (
         <div>
             This is the chat screen
+            {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+            <section>
+                <SendMessage />
+            </section>
         </div>
+    )
+}
+
+function ChatMessage(props) {
+    const { id, text } = props.message
+    return (
+        <p>
+            {text}
+        </p>
     )
 }
 
