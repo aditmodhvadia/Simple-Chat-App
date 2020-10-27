06c6c6d368d943772c76cf0cfb747604e64f68de
Adit Modhvadia <dev.aditmodhvadia@gmail.com>
2020-10-26 22:44:47 -0400

    - add time ago, date and profile photo to msgs

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -3,6 +3,8 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { SendMessage } from '../components/SendMessage';
+import TimeAgo from 'react-timeago';
+import dateFormat from 'dateformat';
 
 function ChatScreen() {
   const chatMessagesRef = firebase.firestore().collection('chatmessages');
@@ -12,7 +14,6 @@ function ChatScreen() {
 
   return (
     <div>
-      This is the chat screen
       {messages &&
         messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
       <section>
@@ -23,8 +24,21 @@ function ChatScreen() {
 }
 
 function ChatMessage(props) {
-  const { text } = props.message;
-  return <p>{text}</p>;
+  const { text, photoURL, createdAt } = props.message;
+  const msgDate = createdAt
+    ? new Date((createdAt.seconds - 4) * 1000 + createdAt.nanoseconds / 1000000)
+    : null;
+  const timeAgo = msgDate ? <TimeAgo date={msgDate} /> : null;
+
+  return (
+    <div>
+      <p> {text}</p>
+      <img src={photoURL} alt='Sender' height='30' width='30' />
+      <br />
+      {timeAgo}
+      <div>{dateFormat(msgDate, 'h:MM TT')}</div>
+    </div>
+  );
 }
 
 export default ChatScreen;
