3605ce007ae3a2cec17ba0c62f123643b845b851
Adit Modhvadia <adit.modhvadia@gmail.com>
2020-10-27 15:01:50 -0400

    - add profanity filter, ban users who send explicit msgs, - update readme

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -7,38 +7,38 @@ import TimeAgo from 'react-timeago';
 import dateFormat from 'dateformat';
 
 function ChatScreen() {
-  const chatMessagesRef = firebase.firestore().collection('chatmessages');
-  const query = chatMessagesRef.orderBy('createdAt').limit(50);
+    const chatMessagesRef = firebase.firestore().collection('chatmessages');
+    const query = chatMessagesRef.orderBy('createdAt').limit(50);
 
-  const [messages] = useCollectionData(query, { idField: 'id' });
+    const [messages] = useCollectionData(query, { idField: 'id' });
 
-  return (
-    <div>
-      {messages &&
-        messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
-      <section>
-        <SendMessage />
-      </section>
-    </div>
-  );
+    return (
+        <div>
+            {messages &&
+                messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+            <section>
+                <SendMessage />
+            </section>
+        </div>
+    );
 }
 
 function ChatMessage(props) {
-  const { text, photoURL, createdAt } = props.message;
-  const msgDate = createdAt
-    ? new Date((createdAt.seconds - 4) * 1000 + createdAt.nanoseconds / 1000000)
-    : null;
-  const timeAgo = msgDate ? <TimeAgo date={msgDate} /> : null;
+    const { text, photoURL, createdAt } = props.message;
+    const msgDate = createdAt
+        ? new Date(createdAt.seconds * 1000 + createdAt.nanoseconds / 1000000)
+        : null;
+    const timeAgo = msgDate ? <TimeAgo date={msgDate} /> : null;
 
-  return (
-    <div>
-      <p> {text}</p>
-      <img src={photoURL} alt='Sender' height='30' width='30' />
-      <br />
-      {timeAgo}
-      <div>{dateFormat(msgDate, 'h:MM TT')}</div>
-    </div>
-  );
+    return (
+        <div>
+            <p> {text}</p>
+            <img src={photoURL} alt='Sender' height='30' width='30' />
+            <br />
+            {timeAgo}
+            <div>{dateFormat(msgDate, 'h:MM TT')}</div>
+        </div>
+    );
 }
 
 export default ChatScreen;
