2f9caeaf9581b90e21e4f3e50db015f78f96de90
Adit Modhvadia <dev.aditmodhvadia@gmail.com>
2020-10-26 21:50:14 -0400

    - fix unused id error

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,35 +1,30 @@
-import React from 'react'
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
-import { SendMessage } from '../components/SendMessage'
-
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { SendMessage } from '../components/SendMessage';
 
 function ChatScreen() {
-    const chatMessagesRef = firebase.firestore().collection('chatmessages')
-    const query = chatMessagesRef.orderBy('createdAt').limit(50)
+  const chatMessagesRef = firebase.firestore().collection('chatmessages');
+  const query = chatMessagesRef.orderBy('createdAt').limit(50);
 
-    const [messages] = useCollectionData(query, { idField: 'id' })
+  const [messages] = useCollectionData(query, { idField: 'id' });
 
-    return (
-        <div>
-            This is the chat screen
-            {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-
-            <section>
-                <SendMessage />
-            </section>
-        </div>
-    )
+  return (
+    <div>
+      This is the chat screen
+      {messages &&
+        messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      <section>
+        <SendMessage />
+      </section>
+    </div>
+  );
 }
 
 function ChatMessage(props) {
-    const { id, text } = props.message
-    return (
-        <p>
-            {text}
-        </p>
-    )
+  const { text } = props.message;
+  return <p>{text}</p>;
 }
 
-export default ChatScreen
+export default ChatScreen;
