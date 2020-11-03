48aec529f7600c7ec5cf5583a80074584c678a41
Adit Modhvadia <adit.modhvadia@gmail.com>
2020-11-03 00:36:20 -0500

    - add facebook login

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -9,10 +9,24 @@ function SignInScreen() {
         const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
         firebase.auth().signInWithPopup(googleAuthProvider)
     }
+
+    const handleFacebookSignIn = () => {
+        console.log("Function called");
+        const facebookAuthProvider = new firebase.auth.FacebookAuthProvider()
+        facebookAuthProvider.addScope('email');
+        // facebookAuthProvider.addScope('profile_photo');
+        firebase.auth().signInWithPopup(facebookAuthProvider).then(res => {
+            console.log(res);
+        }).catch(err => {
+            console.error(err);
+        })
+    }
     return (
         <Container maxWidth="sm">
             <GoogleButton onClick={handleGoogleSignIn} />
-        </Container>
+            <button onClick={handleFacebookSignIn}>Facebook</button>
+            <div className="fb-login-button" data-size="large" data-button-type="login_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="true" data-width="" onClick={handleFacebookSignIn}></div>
+        </Container >
     )
 }
 
